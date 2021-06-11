6e0628208d0e633aaabe328b842de0f136b327ec
Greg Connell <gconnell@160over90.com>
2021-06-11 16:49:07 -0700

    puppeteer is gathering dps feeds/image-reqs

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const argv = require('minimist')(process.argv.slice(2))
 
+const compiler = require('./lib/compiler.js')
 const assets = require('./lib/assets.js')
-// const packager = require('./lib/packager.js')
 
 const debug = require('@ff0000-ad-tech/debug')
 const log = debug('cs-plugin-dps-renderer')
 
-global.api = `http://${argv.api}`
+global.api = argv.api
+
+log({ argv })
+const FOLDERS = JSON.parse(argv.folders)
 
 switch (argv.cmd) {
 	case 'render':
 		const targets = JSON.parse(argv.targets)
-		assets.precompile(argv.profile, targets)
+			; (async () => {
+				// ensure that 3-traffic versions have been compiled
+				await compiler.execute(argv.profile, targets)
+				// run ads in puppeteer to discover dynamic dps assets
+				await assets.preflight(argv.origin, FOLDERS.traffic, targets)
+			})()
 		break
 }
